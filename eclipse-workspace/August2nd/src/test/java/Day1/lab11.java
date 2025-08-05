0518b43fe1b9d50a71ab40102528b5450ca50db9
SandeepKumar172003 <8978sandeepkumar@gmail.com>
2025-08-05 19:41:31 +0530

    Create lab11.java

---
+++ eclipse-workspace/August2nd/src/test/java/Day1/lab11.java
@@ -0,0 +1,26 @@
+package Day1;
+import java.time.LocalDate;
+import java.time.Period;
+import java.util.Scanner;
+//Duration calculator
+public class lab11 {
+
+	public static void main(String[] args) {
+		Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Enter a date ");
+        String input = scanner.nextLine();
+        LocalDate date = LocalDate.parse(input);
+        LocalDate currentDate = LocalDate.now();
+        Period duration = Period.between(date, currentDate);
+    
+        System.out.println("Years  : " +duration.getYears());
+        System.out.println("Months : " + duration.getMonths());
+        System.out.println("Days   : " +duration.getDays());
+
+        scanner.close();
+    
+
+	}
+
+}
