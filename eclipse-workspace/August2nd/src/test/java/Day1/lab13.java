d886a8f43fc88e968dcb830ee52e96c9e55ab189
SandeepKumar172003 <8978sandeepkumar@gmail.com>
2025-08-05 19:46:09 +0530

    Create lab13.java

---
+++ eclipse-workspace/August2nd/src/test/java/Day1/lab13.java
@@ -0,0 +1,26 @@
+package Day1;
+
+import java.time.LocalDate;
+import java.time.Period;
+import java.util.Scanner;
+//Warranty expries
+public class lab13{
+
+    public static void main(String[] args) {
+        Scanner sc= new Scanner(System.in);
+
+        System.out.print("Enter a product purchase date ");
+        String purchasedate = sc.nextLine();
+        LocalDate date = LocalDate.parse(purchasedate);
+        System.out.print("Enter the warratnt years and months");
+        int years=sc.nextInt();
+        int months=sc.nextInt();
+        
+       LocalDate expiryDate=date.plusYears(years).plusMonths(months);
+       
+ 
+        System.out.println("warranty Expiry Date : " +expiryDate);
+
+    
+    }
+}
